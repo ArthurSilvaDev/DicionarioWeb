f2006df1712938587fcee8854aba94c157a9e6fa
ArthFSDev <100644424+ArthurSilvaDev@users.noreply.github.com>
2024-02-05 12:00:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,168 @@
+const palavra = document.getElementById("palavra");
+const imagem = document.getElementById("image")
+const pronuncia= document.getElementById("pronuncia");
+const texto1 = document.getElementById("texto1");
+const texto2 = document.getElementById("texto2");
+
+
+function clickMain(){
+    palavra.innerHTML = "Bem vindo(a), selecione uma letra"
+    imagem.setAttribute('src','../img/main.gif')
+    pronuncia.innerHTML = ""
+    texto1.innerHTML = ""
+    texto2.innerHTML = ""
+}
+
+function clickA(){
+    palavra.innerHTML = "AVOID"
+    imagem.setAttribute('src','../img/a.jpg')
+    pronuncia.innerHTML = "uh·voyd"
+    texto1.innerHTML = "Our responsibility in the Commission today is to <strong>avoid</strong> uncoordinated action"
+    texto2.innerHTML = "We must not make this mistake: we must help each other to <strong>avoid</strong> making this mistake."
+}
+
+function clickB(){
+    palavra.innerHTML = "BUY"
+    imagem.setAttribute('src','../img/b.jpg')
+    pronuncia.innerHTML = "bai"
+    texto1.innerHTML = "He could not afford to <strong>buy</strong> a house."
+    texto2.innerHTML = "I'd like to <strong>buy</strong> him lunch."
+}
+function clickC(){
+    palavra.innerHTML = "charity"
+    imagem.setAttribute('src','../img/c.jpg')
+    pronuncia.innerHTML = "cheh·ruh·tee"
+    texto1.innerHTML = "She was working as a volunteer at a homeless <strong>charity</strong> in Cambridge."
+    texto2.innerHTML = "My mum was very proud. She wouldn't accept <strong>charity</strong>"
+}
+function clickD(){
+    palavra.innerHTML = "disease"
+    imagem.setAttribute('src','../img/d.jpg')
+    pronuncia.innerHTML = "duh·zeez"
+    texto1.innerHTML = "Doctors believe they have cured him of the <strong>disease</strong>."
+    texto2.innerHTML = "High cholesterol can lead to heart <strong>disease</strong>."
+}
+function clickE(){
+    palavra.innerHTML = "environment"
+    imagem.setAttribute('src','../img/e.jpg')
+    pronuncia.innerHTML = "uhn·vai·urn·muhnt"
+    texto1.innerHTML = "Our moral character is formed not by heredity but by <strong>environment</strong>."
+    texto2.innerHTML = "Will central banks have to change their policy settings to suit a dramatically different economic <strong>environment</strong>?"
+}
+function clickF(){
+    palavra.innerHTML = "friendly"
+    imagem.setAttribute('src','../img/f.jpg')
+    pronuncia.innerHTML = "frend·lee"
+    texto1.innerHTML = "Godfrey had been <strong>friendly</strong> to me."
+    texto2.innerHTML = "Your cat isn't very <strong>friendly</strong>."
+}
+function clickG(){
+    palavra.innerHTML = "groceries"
+    imagem.setAttribute('src','../img/g.jpg')
+    pronuncia.innerHTML = "grow·sr·eez"
+    texto1.innerHTML = "One in five reported their parents helped out by paying for <strong>groceries</strong>."
+    texto2.innerHTML = "We know that medical supplies and <strong>groceries</strong> are coming out of the port"
+}
+function clickH(){
+    palavra.innerHTML = "hairbrush"
+    imagem.setAttribute('src','../img/h.jpg')
+    pronuncia.innerHTML = "hehr·bruhsh"
+    texto1.innerHTML = "Where is my <strong>hairbrush</strong>?"
+    texto2.innerHTML = "The <strong>hairbrush</strong> in her hand clattered to the floor."
+}
+
+function clickI(){
+    palavra.innerHTML = "invoice"
+    imagem.setAttribute('src','../img/i.jpg')
+    pronuncia.innerHTML = "in·voys"
+    texto1.innerHTML = "We will then send you an <strong>invoice</strong> for the total course fees."
+    texto2.innerHTML = "His £700 <strong>invoice</strong> was settled immediately in cash."
+}
+
+function clickJ(){
+    palavra.innerHTML = "jinx"
+    imagem.setAttribute('src','../img/j.jpg')
+    pronuncia.innerHTML = "jingks"
+    texto1.innerHTML = "He was beginning to think he was a <strong>jinx</strong>."
+    texto2.innerHTML = "There is no such thing as a <strong>jinx</strong>."
+}
+
+function clickL(){
+    palavra.innerHTML = "lamp"
+    imagem.setAttribute('src','../img/l.jpg')
+    pronuncia.innerHTML = "lamp"
+    texto1.innerHTML = "Turn off the <strong>lamp</strong>."
+    texto2.innerHTML = "I bought a new table <strong>lamp</strong>."
+}
+
+function clickM(){
+    palavra.innerHTML = "meeting"
+    imagem.setAttribute('src','../img/m.jpg')
+    pronuncia.innerHTML = "mi·tin"
+    texto1.innerHTML = "The <strong>meeting</strong> starts at five p.m."
+    texto2.innerHTML = "We're having a <strong>meeting</strong> on Thursday to discuss the problem"
+}
+
+function clickN(){
+    palavra.innerHTML = "nephew"
+    imagem.setAttribute('src','../img/n.jpg')
+    pronuncia.innerHTML = "neh·fyoo"
+    texto1.innerHTML = " My father’s <strong>nephew</strong> is staying with us."
+    texto2.innerHTML = "They were murmuring about the boss's <strong>nephew</strong> getting the job."
+}
+
+function clickO(){
+    palavra.innerHTML = "occasionally"
+    imagem.setAttribute('src','../img/o.jpg')
+    pronuncia.innerHTML = "uh·kay·zhuh·nuh·lee"
+    texto1.innerHTML = "I see him <strong>occasionally</strong> in town."
+    texto2.innerHTML = "<strong>Occasionally</strong> I'll have a piece of chocolate, but it's very rare"
+}
+
+function clickP(){
+    palavra.innerHTML = "property"
+    imagem.setAttribute('src','../img/p.jpg')
+    pronuncia.innerHTML = "praa·pr·tee"
+    texto1.innerHTML = "This <strong>property</strong> belongs to me."
+    texto2.innerHTML = "The club does not accept responsibility for loss of or damage to club members' personal <strong>property</strong>."
+}
+
+function clickQ(){
+    palavra.innerHTML = "quarantine"
+    imagem.setAttribute('src','../img/q.jpg')
+    pronuncia.innerHTML = "kwaw·ruhn·teen"
+    texto1.innerHTML = "The monkeys were kept in <strong>quarantine</strong> for 31 days."
+    texto2.innerHTML = "The doctor said I could go home as long as I agreed to stay in <strong>quarantine</strong> for at least seven days."
+}
+
+function clickR(){
+    palavra.innerHTML = "regularly"
+    imagem.setAttribute('src','../img/r.jpg')
+    pronuncia.innerHTML = "reh·gyoo·lr·lee"
+    texto1.innerHTML = "I do exercise <strong>regularly</strong>"
+    texto2.innerHTML = "She <strong>regularly</strong> appears on TV talk shows."
+}
+
+function clickS(){
+    palavra.innerHTML = "Shelf"
+    imagem.setAttribute('src','../img/s.jpg')
+    pronuncia.innerHTML = "shelf    "
+    texto1.innerHTML = "He took a book from the <strong>shelf</strong>. "
+    texto2.innerHTML = "I hit my head on the <strong>shelf</strong> as I was standing up."
+}
+
+function clickT(){
+    palavra.innerHTML = "Together"
+    imagem.setAttribute('src','../img/t.jpeg')
+    pronuncia.innerHTML = "tuh·geh·thr"
+    texto1.innerHTML = "We used to go to yoga <strong>together</strong>."
+    texto2.innerHTML = "You mix all the dry ingredients <strong>together</strong> before you add the milk."
+}
+
+function clickW(){
+    palavra.innerHTML = "Worry"
+    imagem.setAttribute('src','../img/w.jpg')
+    pronuncia.innerHTML = "wur·ee"
+    texto1.innerHTML = "Don't <strong>worry</strong>, she'll be all right."
+    texto2.innerHTML = "Don't <strong>worry</strong>, I know how to fix the computer."
+}
